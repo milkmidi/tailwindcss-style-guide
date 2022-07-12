a2650865ba6660080c7b525d4cd45bb2f363739d
milkmidi <milkmidi@gmail.com>
2022-07-12 22:52:38 +0800

    feat: preview

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-classes-per-file */
 import './css/app.scss';
 
 class MyCode extends HTMLElement {
@@ -24,3 +25,40 @@ class MyCode extends HTMLElement {
 }
 
 window.customElements.define('my-code', MyCode);
+
+const escapeHTML = (html) => {
+  return html.trim().replaceAll('<', '&lt;').replaceAll('>', '&gt;');
+};
+
+class Preview extends HTMLElement {
+  constructor() {
+    super();
+    const findDataPreviewContainer = this.querySelector('[data-preview]');
+    findDataPreviewContainer.classList.add('hidden', 'data-active:block');
+    findDataPreviewContainer.setAttribute('data-active', 'true');
+    const nodeStr = Array.from(findDataPreviewContainer.childNodes)
+      .map((child) => child.outerHTML)
+      .filter(Boolean);
+
+    const tab = document.createElement('div');
+    tab.innerHTML = `
+      <button class="my-tab">Preview</button>
+      <button class="my-tab">HTML</button>
+    `;
+    requestAnimationFrame(() => {
+      this.querySelectorAll('.my-tab').forEach((tabButton, idx) => {
+        tabButton.addEventListener('click', () => {
+          this.children[1].setAttribute('data-active', 'false');
+          this.children[2].setAttribute('data-active', 'false');
+          this.children[idx + 1].setAttribute('data-active', 'true');
+        });
+      });
+    });
+    const codeChild = document.createElement('div');
+    codeChild.classList.add('code-child', 'hidden', 'data-active:block');
+    codeChild.innerHTML = `<pre class="my-code">${escapeHTML(nodeStr.join('\n'))}</pre>`;
+    this.prepend(tab);
+    this.appendChild(codeChild);
+  }
+}
+window.customElements.define('my-preview', Preview);
